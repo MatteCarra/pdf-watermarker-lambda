af764c8f63998f458738b3eb2c069846e55bb29f
Matte Carra <matte.carra@gmail.com>
2017-08-15 14:38:25 +0200

    First commit

---
+++ src/main/java/com/github/mattecarra/PdfWatermarkService.java
@@ -0,0 +1,97 @@
+package com.github.mattecarra;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.StorageClass;
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.color.Color;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.extgstate.PdfExtGState;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
+
+import java.io.*;
+
+/**
+ * Created by matteo on 09/04/17.
+ */
+public class PdfWatermarkService {
+    private final AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+
+    public static void main(String[] args) throws Throwable {
+        WatermarkInput i = new WatermarkInput();
+        i.setS3OutputKey("1");
+        i.setUser("MatteCarra");
+        i.setLicenseText("Product sold to MatteCarra");
+        PdfWatermarkService s = new PdfWatermarkService();
+        s.handleRequest(i, null);
+    }
+
+    public Integer handleRequest(WatermarkInput watermarkInput, Context context) throws Throwable {
+        String BUCKET = System.getenv("S3_BUCKET");
+
+        S3Object s3Object = s3.getObject(BUCKET, watermarkInput.getS3InputFileKey());
+
+        File file = null;
+        try {
+            file = File.createTempFile("watermarked", ".pdf");
+
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(s3Object.getObjectContent()), new PdfWriter(file));
+            Document doc = new Document(pdfDoc);
+            int n = pdfDoc.getNumberOfPages();
+            PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+            Paragraph p = new Paragraph(watermarkInput.getLicenseText()).setFont(font).setFontSize(30).setFontColor(Color.BLUE);
+            Paragraph invisibleP = new Paragraph("" + watermarkInput.getUser()).setFontSize(1);
+
+            // transparency
+            PdfExtGState gs1 = new PdfExtGState();
+            gs1.setFillOpacity(0.1f);
+
+            PdfExtGState gs2 = new PdfExtGState();
+            gs2.setFillOpacity(0f);
+
+            PdfCanvas over;
+            Rectangle pagesize;
+            float x, y;
+
+            for(int i = 1; i <= n; i ++){
+                PdfPage pdfPage = pdfDoc.getPage(i);
+                pagesize = pdfPage.getPageSizeWithRotation();
+                pdfPage.setIgnorePageRotationForContent(true);
+                x = (pagesize.getLeft() + pagesize.getRight()) / 2;
+                y = (pagesize.getTop() + pagesize.getBottom()) / 2;
+
+                over = new PdfCanvas(pdfDoc.getPage(i));
+                over.saveState();
+                over.setExtGState(gs1);
+                doc.showTextAligned(p, x, y, i, TextAlignment.CENTER, VerticalAlignment.TOP, (float) Math.toRadians(45));
+
+                over.saveState();
+                over.setExtGState(gs2);
+                doc.showTextAligned(invisibleP, pagesize.getRight(), pagesize.getBottom(), i, TextAlignment.RIGHT, VerticalAlignment.BOTTOM, 0);
+
+                over.restoreState();
+            }
+
+            doc.close();
+
+            String objectKey = watermarkInput.getS3OutputKey();
+            s3.putObject(new PutObjectRequest(BUCKET, objectKey, file).withStorageClass(StorageClass.ReducedRedundancy));
+            return 1;
+        } finally {
+            if(file != null) file.delete();
+        }
+    }
+}
---
+++ src/main/java/com/github/mattecarra/WatermarkInput.java
@@ -0,0 +1,43 @@
+package com.github.mattecarra;
+
+/**
+ * Created by matteo on 09/04/17.
+ */
+public class WatermarkInput {
+    private String s3InputFileKey;
+    private String s3OutputKey;
+    private String user;
+    private String licenseText;
+
+    public String getS3InputFileKey() {
+        return s3InputFileKey;
+    }
+
+    public void setS3InputFileKey(String s3InputFileKey) {
+        this.s3InputFileKey = s3InputFileKey;
+    }
+
+    public String getS3OutputKey() {
+        return s3OutputKey;
+    }
+
+    public void setS3OutputKey(String s3OutputKey) {
+        this.s3OutputKey = s3OutputKey;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getLicenseText() {
+        return licenseText;
+    }
+
+    public void setLicenseText(String licenseText) {
+        this.licenseText = licenseText;
+    }
+}
